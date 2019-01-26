36d51f9f62b1cb96c250afa2a5629b7ad3042ce4
rafakwas <ralphigi@gmail.com>
2019-01-26 12:56:44 +0100

    auth

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show]
+  before_action :check_logged_in, :only => [:new, :create]
   
   def index
     @events = Event.all
@@ -28,4 +29,10 @@ class EventsController < ApplicationController
     def set_event
       @event = Event.find(params[:id])
     end
+    
+    def check_logged_in
+			authenticate_or_request_with_http_basic("Ads") do |username, password| 
+					username == "admin" && password == "admin"
+			end
+		end
 end
